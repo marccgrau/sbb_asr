f214e533267f867d490edf23184f3b1a0890778d
marccgrau <marcgrau@outlook.com>
2022-12-05 17:07:54 +0000

    all samples involved in general folder

--- src/sbb_project/consts.py
+++ src/sbb_project/consts.py
@@ -8,7 +8,7 @@ PROJ_DIR = Path('/home/user/code/sbb_asr')
 DATA_DIR = PROJ_DIR.joinpath('data')
 SBB_DATA_EXCHANGE_DIR = DATA_DIR.joinpath('sbb_exchange')
 # Set current batch sent as directory
-SBB_DATA_EXCHANGE_CURRENT_SAMPLE = SBB_DATA_EXCHANGE_DIR.joinpath('samples_2022_11_29')
+SBB_DATA_EXCHANGE_CURRENT_SAMPLE = SBB_DATA_EXCHANGE_DIR.joinpath('all_samples')
 SBB_DATA_EXCHANGE_AUDIO = SBB_DATA_EXCHANGE_CURRENT_SAMPLE.joinpath('audios')
 SBB_DATA_EXCHANGE_LABELS = SBB_DATA_EXCHANGE_CURRENT_SAMPLE.joinpath('labels')
 # Manifests
