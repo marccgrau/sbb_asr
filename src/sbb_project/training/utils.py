0a3a79fb742a737837951941be9a7ced560ba659
marccgrau <marcgrau@outlook.com>
2022-12-05 17:17:00 +0000

    secure reproducbility across manifest creation

--- src/sbb_project/training/utils.py
+++ src/sbb_project/training/utils.py
@@ -1,11 +1,9 @@
 import torch
 import numpy as np
 
-def reproducibility(config):
-    SEED = int(config.seed)
+def reproducibility(SEED: int):
     torch.manual_seed(SEED)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
     np.random.seed(SEED)
-    if config.cuda:
-        torch.cuda.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
