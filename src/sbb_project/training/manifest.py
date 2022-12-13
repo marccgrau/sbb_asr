696af11dec3f6f6756f28f6c2a90c9b6fda4e8b3
marccgrau <marcgrau@outlook.com>
2022-12-13 12:34:04 +0000

    noise pipeline to create audio, adjusted manifest creation to create noisy data manifests

--- src/sbb_project/training/manifest.py
+++ src/sbb_project/training/manifest.py
@@ -6,12 +6,21 @@ from sklearn import model_selection
 from sbb_project import consts
 import string
 
-def convert_sbb_json_to_nvidia(file):
+def convert_sbb_json_to_nvidia(file, snr = None):
     f = open(file)
     data = json.load(f)
     sampleId = data['sampleId']
-    audio = '{}.wav'.format(sampleId)
-    audio_path = consts.SBB_DATA_EXCHANGE_AUDIO.joinpath(audio)
+    
+    if snr is None:
+        audio = '{}.wav'.format(sampleId)
+        audio_path = consts.SBB_DATA_EXCHANGE_AUDIO.joinpath(audio)
+    elif snr == -10:
+        audio = 'snr_neg10_{}.wav'.format(sampleId)
+        audio_path = consts.SBB_DATA_EXCHANGE_DIR.joinpath('snr_neg10_samples')
+    else:
+        audio = 'snr_{}_{}.wav'.format(snr, sampleId)
+        audio = consts.SBB_DATA_EXCHANGE_DIR.joinpath('snr_{}_samples'.format(snr))
+    
     sentence = data['sentence'].lower().translate(str.maketrans('', '', string.punctuation))
     duration = librosa.core.get_duration(filename=audio_path)
     metadata = {
